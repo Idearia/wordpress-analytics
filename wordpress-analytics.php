94221736469962e7cb59add3c87daef026f90c53
Guido W. Pettinari <guido.pettinari@gmail.com>
2016-08-04 20:02:54 +0200

    Fixed inconsequential bug in scroll tracking

--- wordpress-analytics.php
+++ wordpress-analytics.php
@@ -43,10 +43,6 @@
     if ( isset ( $options ['scroll_tracking'] ) && $options ['scroll_tracking'] )
       require_once ( WPAN_PLUGIN_DIR . 'scroll_tracking.php' );
 
-    /* Load scroll tracking function */
-    if ( isset ( $options ['scroll_tracking'] ) && $options ['scroll_tracking'] )
-      require_once ( WPAN_PLUGIN_DIR . 'scroll_tracking.php' );
-
     /* Load call tracking function */
     if ( isset ( $options ['call_tracking'] ) && $options ['call_tracking'] )
       require_once ( WPAN_PLUGIN_DIR . 'call_tracking.php' );
