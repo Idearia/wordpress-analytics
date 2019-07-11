7e649f52a71a34c022be8a0542f8ae6d55ab715c
Guido W. Pettinari <guido.pettinari@gmail.com>
2019-07-11 20:17:47 +0200

    Sniffed form_Tracking.php

--- form_tracking.php
+++ form_tracking.php
@@ -1,116 +1,121 @@
 <?php
 
-  /**
-   * Track forms submissions in Google Analytics using the Measurement
-   * Protocol.
-   *
-   * Created by Guido W. Pettinari on 04.08.2016.
-   * Part of WordPress Analytics:
-   * https://github.com/coccoinomane/wordpress_analytics
-   */
+/**
+ * Track forms submissions in Google Analytics using the Measurement
+ * Protocol.
+ *
+ * Created by Guido W. Pettinari on 04.08.2016.
+ * Part of WordPress Analytics:
+ * https://github.com/coccoinomane/wordpress_analytics
+ */
 
-  /* Add Gravity Forms filter */
-  if ( class_exists( 'GFCommon' ) ) {
+// Add Gravity Forms filter
+if ( class_exists( 'GFCommon' ) ) {
 
-    /* Load the server-side library to send hits to Google Analytics */
-    if ( ! defined( 'WPAN_GAMP_LOADED' ) ) {
+	/* Load the server-side library to send hits to Google Analytics */
+	if ( ! defined( 'WPAN_GAMP_LOADED' ) ) {
 
-      if ( wpan_load_measurement_protocol_client () ) {
+		if ( wpan_load_measurement_protocol_client() ) {
 
-        /* Send data to GA only after form validation & submission */
-        add_action( 'gform_after_submission', 'wpan_send_form_tracking_event_wrapper', 10, 4 );
-        
-      }
+			/* Send data to GA only after form validation & submission */
+			add_action( 'gform_after_submission', 'wpan_send_form_tracking_event_wrapper', 10, 4 );
 
-    }
+		}
+	}
 
-    /**
-     * Wrapper to handle errors
-     */
-    function wpan_send_form_tracking_event_wrapper( $entry, $form ) {
-      try {
-        wpan_send_form_tracking_event( $entry, $form );
-      } catch (\Throwable $e) {
-        error_log( "Errore in WordPress Analytics Form Tracking: " . $e->getMessage());
-      }
-    }
+	/**
+	 * Wrapper to handle errors
+	 *
+	 * @param Array $entry
+	 * @param Array $form
+	 */
+	function wpan_send_form_tracking_event_wrapper( $entry, $form ) {
+		try {
+			wpan_send_form_tracking_event( $entry, $form );
+		} catch ( \Throwable $e ) {
+			error_log( 'Errore in WordPress Analytics Form Tracking: ' . $e->getMessage() );
+		}
+	}
 
-    /**
-     * Send an event to GA with details about the submission
-     */
-    function wpan_send_form_tracking_event( $entry, $form ) {
+	/**
+	 * Send an event to GA with details about the submission
+	 *
+	 * @param Array $entry
+	 * @param Array $form
+	 */
+	function wpan_send_form_tracking_event( $entry, $form ) {
 
-      global $post;
+		global $post;
 
-      $msg = "About to send form tracking event to Google Analytics...";
-      wpan_debug( $msg );
-      GFCommon::log_debug( $msg );
+		$msg = 'About to send form tracking event to Google Analytics...';
+		wpan_debug( $msg );
+		GFCommon::log_debug( $msg );
 
-      /* Extract the plugin options from the database */
-      $options = wpan_get_options ();
-      $tracking_uid = isset ( $options ['tracking_uid'] ) ? $options ['tracking_uid'] : '';
-      $debug = isset ( $options['debug'] ) ? $options['debug'] : '';
-      $form_title = $form['title'];
+		/* Extract the plugin options from the database */
+		$options      = wpan_get_options();
+		$tracking_uid = isset( $options ['tracking_uid'] ) ? $options ['tracking_uid'] : '';
+		$debug        = isset( $options['debug'] ) ? $options['debug'] : '';
+		$form_title   = $form['title'];
 
-      /* I have taken the following four lines of code from
-      https://github.com/theiconic/php-ga-measurement-protocol;
-      thank you! */
-      $document_path = parse_url( $entry['source_url'], PHP_URL_PATH );
-      $document_location = 'http' . ( isset( $_SERVER['HTTPS'] ) ? 's' : '' ) . '://' . $_SERVER['HTTP_HOST'] . '/' . $_SERVER['REQUEST_URI'];
-      $document_title = isset( $post ) && get_the_title( $post ) ? get_the_title( $post ) : 'no title';
+		// I have taken the following four lines of code from
+		// https://github.com/theiconic/php-ga-measurement-protocol
+		// Thank you!
+		$document_path     = wp_parse_url( $entry['source_url'], PHP_URL_PATH );
+		$document_location = 'http' . ( isset( $_SERVER['HTTPS'] ) ? 's' : '' ) . '://' . $_SERVER['HTTP_HOST'] . '/' . $_SERVER['REQUEST_URI']; // phpcs:ignore
+		$document_title    = isset( $post ) && get_the_title( $post ) ? get_the_title( $post ) : 'no title';
 
-      /* Setup the class */
-      $ga_options = [
-        'client_create_random_id' => true, // create random client id if the class can't fetch the current cliend id
-        'client_fallback_id' => 555, // fallback client id when cid was not found and random client id is off
-        'client_id' => null, // override client id
-        'user_id' => null, // determine current user id
-        'adapter' => [ // adapter options
-          'async' => true, // requests to google are async - don't wait for google server response
-          'ssl' => false // use ssl connection to google server
-        ]
-      ];
+		/* Setup the class */
+		$ga_options = [
+			'client_create_random_id' => true, // create random client id if the class can't fetch the current cliend id
+			'client_fallback_id'      => 555, // fallback client id when cid was not found and random client id is off
+			'client_id'               => null, // override client id
+			'user_id'                 => null, // determine current user id
+			'adapter'                 => [ // adapter options
+				'async' => true, // requests to google are async - don't wait for google server response
+				'ssl'   => false, // use ssl connection to google server
+			],
+		];
 
-      /* Allow the user to filter the GA options */
-      $event = apply_filters( 'wpan_filter_form_tracking_options', $ga_options, $entry, $form );
+		/* Allow the user to filter the GA options */
+		$event = apply_filters( 'wpan_filter_form_tracking_options', $ga_options, $entry, $form );
 
-      /* Connect to tracker */
-      $gatracking = new \Racecore\GATracking\GATracking( $tracking_uid, $ga_options );
+		/* Connect to tracker */
+		$gatracking = new \Racecore\GATracking\GATracking( $tracking_uid, $ga_options );
 
-      /* Build GA event */
-      $event = $gatracking->createTracking( 'Event' );
-      $event->setAsNonInteractionHit( false );
-      $event->setEventCategory( 'Contact' );
-      $event->setEventAction( 'form:' . $form_title );
-      $event->setEventLabel( $document_path );
-      $event->setDocumentPath( $document_path );
-      $event->setDocumentLocation( $document_location );
-      $event->setDocumentTitle( $document_title );
+		/* Build GA event */
+		$event = $gatracking->createTracking( 'Event' );
+		$event->setAsNonInteractionHit( false );
+		$event->setEventCategory( 'Contact' );
+		$event->setEventAction( 'form:' . $form_title );
+		$event->setEventLabel( $document_path );
+		$event->setDocumentPath( $document_path );
+		$event->setDocumentLocation( $document_location );
+		$event->setDocumentTitle( $document_title );
 
-      /* Allow the user to filter the event; if false, the event will not be sent */
-      $event = apply_filters( 'wpan_filter_form_tracking_event', $event, $gatracking, $entry, $form );
+		/* Allow the user to filter the event; if false, the event will not be sent */
+		$event = apply_filters( 'wpan_filter_form_tracking_event', $event, $gatracking, $entry, $form );
 
-      /* Send event to GA severs */
-      if ( $event )
-        $response = $gatracking->sendTracking( $event );
+		/* Send event to GA severs */
+		if ( $event ) {
+			$response = $gatracking->sendTracking( $event );
+		}
 
-      /* Allow the user to send more events with the same tracker */
-      do_action( 'wpan_action_post_send_form_tracking_event', $gatracking, $entry, $form );
+		/* Allow the user to send more events with the same tracker */
+		do_action( 'wpan_action_post_send_form_tracking_event', $gatracking, $entry, $form );
 
-      /* Debug */
-      if ( $debug ) {
-        wpan_debug( "Sent the following event to Google Analytics:" );
-        wpan_debug( $event ? $event : "Event empty because of filter" );
-        wpan_debug( "Received the following respons from Google Analytics (ASYNC, so it might be empty): ");
-        wpan_debug( $response );
-        // wpan_debug( "This is the form that triggered the event:" );
-        // wpan_debug( $form );
-        // wpan_debug( "This is the entry of the form in Gravity Forms:" );
-        // wpan_debug( $entry );
-      }
+		/* Debug */
+		if ( $debug ) {
+			wpan_debug( 'Sent the following event to Google Analytics:' );
+			wpan_debug( $event ? $event : 'Event empty because of filter' );
+			wpan_debug( 'Received the following respons from Google Analytics (ASYNC, so it might be empty): ' );
+			wpan_debug( $response );
+			// wpan_debug( "This is the form that triggered the event:" );
+			// wpan_debug( $form );
+			// wpan_debug( "This is the entry of the form in Gravity Forms:" );
+			// wpan_debug( $entry );
+		}
 
-    }
-    
-  }
+	}
+}
 
-?>
+
